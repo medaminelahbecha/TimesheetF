96f94988b3f44248d3df11b5f3080a6f9326f0d2
unknown <mohamedamine.lahbecha@esprit.tn>
2020-11-04 17:00:57 +0100

    rectification

--- src/test/java/tn/esprit/spring/EmployerTest.java
+++ src/test/java/tn/esprit/spring/EmployerTest.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
+
 
 import tn.esprit.spring.entities.Contrat;
 import tn.esprit.spring.entities.Departement;
@@ -36,109 +36,123 @@ public class EmployerTest {
 	@Autowired
 	ContratRepository contratRepoistory;
 
-	/*
-	 * @Test public void testAddEmployer() { Employe employer = new Employe("amine",
-	 * "lahbecha", "amine@gmail.com", true, Role.INGENIEUR) ; int employerAdd =
-	 * employeService.addOrUpdateEmploye(employer); assertEquals(employer.getId(),
-	 * employerAdd); }
-	 * 
-	 * 
-	 * @Test public void testUpdateEmploye() { Employe employer = new Employe(1,
-	 * "mohamed amine", "lahbecha", "amine@gmail.com", "12345", true,
-	 * Role.INGENIEUR); int employerAdd =
-	 * employeService.addOrUpdateEmploye(employer); assertEquals(employer.getId(),
-	 * employerAdd); }
-	 */
-	/*
-	 * @Test public void testRetreveAllEmployes() { List<Employe> listEmployes =
-	 * employeService.getAllEmployes(); assertEquals(2, listEmployes.size()); }
-	 */
+	@Test
+	public void testAddEmployer() {
+		Employe employer = new Employe("amine", "lahbecha", "amine@gmail.com", true, Role.INGENIEUR);
+		int employerAdd = employeService.addOrUpdateEmploye(employer);
+		assertEquals(employer.getId(), employerAdd);
+	}
 
-	/*
-	 * @Test public void AuthentificationTest() { Employe authenticateEmploye =
-	 * employeService.authenticate("amine@gmail.com", "12345"); assertEquals(1,
-	 * authenticateEmploye.getId()); }
-	 */
+	@Test
+	public void testUpdateEmploye() {
+		Employe employer = new Employe(1, "mohamed amine", "lahbecha", "amine@gmail.com", "12345", true,
+				Role.INGENIEUR);
+		int employerAdd = employeService.addOrUpdateEmploye(employer);
+		assertEquals(employer.getId(), employerAdd);
+	}
 
-	/*
-	 * @Test public void mettreAjourEmailByEmployeIdTest() { Employe employe =
-	 * employeService.mettreAjourEmailByEmployeId("amine@esprit.tn", 2);
-	 * assertEquals("amine@esprit.tn", employe.getEmail()); }
-	 */
+	@Test
+	public void testRetreveAllEmployes() {
+		List<Employe> listEmployes = employeService.getAllEmployes();
+		assertEquals(2, listEmployes.size());
+	}
 
-	/*
-	 * @Test public void affecterEmployeADepartementTest() {
-	 * employeService.affecterEmployeADepartement(1, 1); Departement
-	 * depManagedEntity = deptRepoistory.findById(1).get(); int
-	 * nbrEmployeParDepartement = depManagedEntity.getEmployes().size();
-	 * assertEquals(3, nbrEmployeParDepartement); }
-	 */
+	@Test
+	public void AuthentificationTest() {
+		Employe authenticateEmploye = employeService.authenticate("amine@gmail.com", "12345");
+		assertEquals(1, authenticateEmploye.getId());
+	}
+
+	@Test
+	public void mettreAjourEmailByEmployeIdTest() {
+		Employe employe = employeService.mettreAjourEmailByEmployeId("amine@esprit.tn", 2);
+		assertEquals("amine@esprit.tn", employe.getEmail());
+	}
+
+	@Test
+	public void affecterEmployeADepartementTest() {
+		employeService.affecterEmployeADepartement(1, 1);
+		Departement depManagedEntity = deptRepoistory.findById(1).get();
+		int nbrEmployeParDepartement = depManagedEntity.getEmployes().size();
+		assertEquals(3, nbrEmployeParDepartement);
+	}
+
+	@Test
+	public void desaffecterEmployeDuDepartementTest() {
+		employeService.desaffecterEmployeDuDepartement(1, 1);
+		Departement depManagedEntity = deptRepoistory.findById(1).get();
+		int nbrEmployeParDepartement = depManagedEntity.getEmployes().size();
+		assertEquals(1, nbrEmployeParDepartement);
+
+	}
+
+	@Test
+	public void ajouterContratTest() throws ParseException {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		Date dateDebut = dateFormat.parse("2015-03-23");
+		Contrat contrat = new Contrat(dateDebut, "CDI", 1500);
+		employeService.ajouterContrat(contrat);
+		List<Contrat> contratList = (List<Contrat>) contratRepoistory.findAll();
+		assertEquals(1, contratList.size());
+	}
 
 	
 
-	/*
-	 * @Test public void desaffecterEmployeDuDepartementTest() {
-	 * employeService.desaffecterEmployeDuDepartement(1, 1); Departement
-	 * depManagedEntity = deptRepoistory.findById(1).get(); int
-	 * nbrEmployeParDepartement = depManagedEntity.getEmployes().size();
-	 * assertEquals(1, nbrEmployeParDepartement);
-	 * 
-	 * }
-	 */
-	/*
-	 * @Test public void ajouterContratTest() throws ParseException {
-	 * SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd"); Date
-	 * dateDebut = dateFormat.parse("2015-03-23"); Contrat contrat = new
-	 * Contrat(dateDebut, "CDI", 1500); employeService.ajouterContrat(contrat);
-	 * List<Contrat> contratList = (List<Contrat>) contratRepoistory.findAll();
-	 * assertEquals(1, contratList.size()); }
-	 */
-	
-	
-	@SuppressWarnings("deprecation")
-	/*
-	 * @Test public void affecterContratAEmployeTest() {
-	 * employeService.affecterContratAEmploye(1, 1); Contrat contrat =
-	 * contratRepoistory.findById(1).get(); assertEquals(1,
-	 * contrat.getEmploye().getId()); }
-	 */
-	/*
-	 * @Test public void getEmployePrenomById() { String employe =
-	 * employeService.getEmployePrenomById(2); assertEquals("lahbecha", employe);
-	 * 
-	 * }
-	 * 
-	 * @Test public void deleteEmployeByIdTest() {
-	 * employeService.deleteEmployeById(3); List<Employe> empl = (List<Employe>)
-	 * employeRepository.findAll(); assertEquals(2, empl.size());
-	 * 
-	 * 
-	 * }
-	 * 
-	 * @Test public void deleteContratByIdTest() {
-	 * employeService.deleteContratById(2); List<Contrat> contrat = (List<Contrat>)
-	 * contratRepoistory.findAll(); assertEquals(1, contrat.size()); }
-	 * 
-	 * @Test public void getNombreEmployeJPQLTest() { int nbr
-	 * =employeService.getNombreEmployeJPQL(); assertEquals(2, nbr);
-	 * 
-	 * }
-	 * 
-	 * 
-	 * @Test public void getAllEmployeNamesJPQLTest() { List<String> emp
-	 * =employeService.getAllEmployeNamesJPQL(); assertEquals(2, emp); }
-	 * 
-	 * @Test public void deleteAllContratJPQLTset() {
-	 * employeService.deleteAllContratJPQL(); List<Contrat> contrat =
-	 * (List<Contrat>) contratRepoistory.findAll(); assertEquals(1, contrat.size());
-	 * 
-	 * }
-	 */
+	@Test
+	public void affecterContratAEmployeTest() {
+		employeService.affecterContratAEmploye(1, 1);
+		Contrat contrat = contratRepoistory.findById(1).get();
+		assertEquals(1, contrat.getEmploye().getId());
+	}
+
+	@Test
+	public void getEmployePrenomById() {
+		String employe = employeService.getEmployePrenomById(2);
+		assertEquals("lahbecha", employe);
+
+	}
+
+	@Test
+	public void deleteEmployeByIdTest() {
+		employeService.deleteEmployeById(3);
+		List<Employe> empl = (List<Employe>) employeRepository.findAll();
+		assertEquals(2, empl.size());
+
+	}
+
+	@Test
+	public void deleteContratByIdTest() {
+		employeService.deleteContratById(2);
+		List<Contrat> contrat = (List<Contrat>) contratRepoistory.findAll();
+		assertEquals(1, contrat.size());
+	}
+
+	@Test
+	public void getNombreEmployeJPQLTest() {
+		int nbr = employeService.getNombreEmployeJPQL();
+		assertEquals(2, nbr);
+
+	}
+
+	@Test
+	public void getAllEmployeNamesJPQLTest() {
+		List<String> emp = employeService.getAllEmployeNamesJPQL();
+		assertEquals(2, emp);
+	}
+
+	@Test
+	public void deleteAllContratJPQLTset() {
+		employeService.deleteAllContratJPQL();
+		List<Contrat> contrat = (List<Contrat>) contratRepoistory.findAll();
+		assertEquals(1, contrat.size());
+
+	}
+
 	@Test
 	public void getSalaireByEmployeIdJPQLTest() {
-		
+
 		float salaire = employeService.getSalaireByEmployeIdJPQL(1);
-		assertEquals(1500, salaire,0 );
-		
+		assertEquals(1500, salaire, 0);
+
 	}
 }
